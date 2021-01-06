0455062b285e6640905d8c49b876d470cbbbdaf6
Gregg Wong <griekup@live.com>
2021-01-05 23:07:44 -0500

    Transition between components

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,14 +1,36 @@
 <template>
-  <Navbar />
-  <router-view />
+<Navbar />
+<router-view v-slot="{ Component }">
+    <transition name="slide-fade" mode="out-in">
+        <component :is="Component" />
+    </transition>
+</router-view>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar.vue'
 export default {
-  name: 'Layout',
-  components: {
-    Navbar
-  }
+    name: 'Layout',
+    components: {
+        Navbar
+    }
 };
 </script>
+
+<style scoped>
+/*** TRANSITIONS ***/
+.slide-fade-enter {
+    transform: translateX(10px);
+    opacity: 0;
+}
+
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+    transition: all .3s ease;
+}
+
+.slide-fade-leave-to {
+    transform: translateX(-10px);
+    opacity: 0;
+}
+</style>
