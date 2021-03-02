d3a02e0d400243d3b0df8bd711010ce2ad4c1a07
Gregg Wong <griekup@live.com>
2021-03-02 08:50:20 -0500

    Add auth service

---
+++ src/services/AuthService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const apiClient = axios.create({
+    baseURL: "https://localhost:6001/api/auth",
+    withCredentials: false,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
+});
+
+export default {
+    register(userCreds) {
+        return apiClient.post("/register", userCreds);
+    },
+    login(userCreds) {
+        return apiClient.post("/register", userCreds);
+    }
+}
