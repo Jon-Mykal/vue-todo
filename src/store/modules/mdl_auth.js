386aa0abef9d8920c73180b70f53974f1f806047
Gregg Wong <griekup@live.com>
2021-03-02 08:50:46 -0500

    Create Vuex Module for Auth

---
+++ src/store/modules/mdl_auth.js
@@ -0,0 +1,18 @@
+import {AuthService as authSvc}  from '@/services/AuthService.js'
+
+export const namespaced = true;
+export const state = {
+    user: null
+}
+
+export const mutations = {
+    
+}
+
+export const actions = {
+    
+}
+
+export const getters = {
+    
+}
