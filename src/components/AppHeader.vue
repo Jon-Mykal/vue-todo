f969b35db02e364a708d658833f41e5e5c641435
Gregg Wong <griekup@live.com>
2021-04-06 10:41:24 -0500

    Built out tests

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,10 +5,12 @@
 </template>
 
 <script>
+import { ref} from 'vue'
 export default {
     data() {
+        let loggedIn = false ;
         return {
-            loggedIn: false
+            loggedIn
         }
     }
 }
--- tests/unit/AppHeader.spec.js
+++ tests/unit/AppHeader.spec.js
@@ -1,9 +1,23 @@
 // Import component to be tested
 import AppHeader from '@/components/AppHeader.vue';
+import { mount } from '@vue/test-utils' // Allows component to be mounted
 
+// This 'describe' function sets up a test suite
 describe('AppHeader', () => {
+    // Jest test function.
     test('If user is not logged in, do not show logout button', () => {
-        let actual = true;
+        const wrapper = mount(AppHeader);
+        let actual = wrapper.find('button').isVisible();
+        let expected = false;
+        expect(actual).toBe(expected);
+    })
+
+    test('If user is logged in, show logout button', async () => {
+        const wrapper = mount(AppHeader);
+        //wrapper.setData({ loggedIn.})
+        await wrapper.setData({ loggedIn: true });
+       // await wrapper.vm.$nextTick();
+        let actual = wrapper.find('button').isVisible();
         let expected = true;
         expect(actual).toBe(expected);
     })
