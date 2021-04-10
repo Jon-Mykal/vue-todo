d5f4751f731795da269a07d892b70690b4884c21
Gregg Wong <griekup@live.com>
2021-04-10 16:52:01 -0500

    Testing props and interactions

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,16 +1,36 @@
 <template>
     <div>
-        <button v-show="loggedIn">Logout</button>
+        <span>{{ randomNumber }}</span>
+        <button @click="getRandomNumber" id="btnGen">Generate Random Number</button>
+        <button v-show="loggedIn" id="btnLog">Logout</button>
     </div>
 </template>
 
 <script>
 import { ref} from 'vue'
 export default {
+    props: {
+        min: {
+            type: Number,
+            default: 1
+        },
+        max: {
+            type: Number,
+            default: 10
+        }
+    },
     data() {
-        let loggedIn = false ;
+        let loggedIn = false;
+        let randomNumber = 0;
         return {
-            loggedIn
+            loggedIn,
+            randomNumber
+        }
+    },
+    methods: {
+        getRandomNumber() {
+            this.randomNumber =
+                Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
         }
     }
 }
--- tests/unit/AppHeader.spec.js
+++ tests/unit/AppHeader.spec.js
@@ -7,10 +7,10 @@ describe('AppHeader', () => {
     // Jest test function.
     test('If user is not logged in, do not show logout button', () => {
         const wrapper = mount(AppHeader);
-        let actual = wrapper.find('button').isVisible();
+        let actual = wrapper.find('#btnLog').isVisible();
         let expected = false;
         expect(actual).toBe(expected);
-    })
+    });
 
     test('If user is logged in, show logout button', async () => {
         const wrapper = mount(AppHeader);
@@ -20,5 +20,33 @@ describe('AppHeader', () => {
         let actual = wrapper.find('button').isVisible();
         let expected = true;
         expect(actual).toBe(expected);
+    });
+
+    test("By default, randomNumber data value should be 0", () => {
+        const wrapper = mount(AppHeader);
+        expect(wrapper.html()).toContain('<span>0</span>');
     })
+
+    test("By default, randomNumber data value should be between 1 and 10", async () => {
+        const wrapper = mount(AppHeader);
+        // Use Async/Await to ensure the DOM updates
+        await wrapper.find("#btnGen").trigger('click');
+        const randomNumber = parseInt(wrapper.find('span').element.textContent);
+        expect(randomNumber).toBeGreaterThanOrEqual(1);
+        expect(randomNumber).toBeLessThanOrEqual(10);
+    })
+
+    test("If button is clicked, randomNumber should be between 200 and 300", async() => {
+        const wrapper = mount(AppHeader, {
+            propsData: {
+                min: 200,
+                max: 300
+            }
+        });
+        // Use Async/Await to ensure the DOM updates
+        await wrapper.find("#btnGen").trigger('click');
+        const randomNumber = parseInt(wrapper.find('span').element.textContent);
+        expect(randomNumber).toBeGreaterThanOrEqual(200);
+        expect(randomNumber).toBeLessThanOrEqual(300);
+    });
 })
